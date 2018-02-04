8b316534c2a8a6ac3cc707038210954609277a00
wavesaber <wavesaber@gmail.com>
2018-02-04 17:45:48 +0100

    Commited other stuff

---
+++ app.js
@@ -0,0 +1,13 @@
+web3mystuff = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/z0u3VgomCEuiUbIKpk9y"))
+
+abi = JSON.parse('[ { "constant": false, "inputs": [ { "name": "newText", "type": "string" } ], "name": "setText", "outputs": [ { "name": "", "type": "bool" } ], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [], "name": "mostSent", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "withdrawEther", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [ { "name": "", "type": "address", "value": "0xd2b59245bf7fdcbb356be11b19246bcc71cea698" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "currentText", "outputs": [ { "name": "", "type": "string", "value": "Put your own text here for money!" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "maxLength", "outputs": [ { "name": "", "type": "uint256", "value": "50" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" } ]')
+contractInstance = web3mystuff.eth.contract(abi).at('0x06eA246C7Ea2E6952c65f50e1b00059aAC626753');
+
+const currentText=document.getElementById('currentText'); 
+const mostSent=document.getElementById('mostSent')
+
+const mostSentContract = contractInstance.mostSent() / 10**18
+
+mostSent.innerHTML =  "Current price: " + (mostSentContract).toString() + " ETH"
+currentText.innerHTML = "\"" + contractInstance.currentText() + "\""
+amount.value = mostSentContract + 0.01
